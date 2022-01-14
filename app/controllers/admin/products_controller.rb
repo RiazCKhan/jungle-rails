31d5d0c2f48ee9f64dfb1c17d847fd1d095ef609
RiazK <RiazCKhan@gmail.com>
2022-01-14 15:44:36 -0500

    Add basic HTTP Auth

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: "Jungle", password: "book", only: :index
+
   def index
     @products = Product.order(id: :desc).all
   end
