9018b90cdd5a5de9fb51d95af47b03f5d86e27be
RiazK <RiazCKhan@gmail.com>
2022-01-15 13:04:00 -0500

    Add instance variables to count products and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,13 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], only: :show
 
   def show
+    # active record = COUNT # products in DB
+    @number_of_products = Product.count
+    
+    # active record = COUNT # of categores in DB 
+    @number_of_categories = Category.count
   end
 end
+
+# Use an @instance_variablein the controllers 'action' (=def show)
+# render the values in the ERB template
