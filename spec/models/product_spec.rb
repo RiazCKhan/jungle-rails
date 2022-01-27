2509ef567720e650d525f2a0602b21b5b3ceb7ea
RiazK <RiazCKhan@gmail.com>
2022-01-27 17:13:38 -0500

    Add product name and price test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -19,10 +19,23 @@ RSpec.describe Product, type: :model do
       expect(@product).to be_present
     end
 
+    it "absence of product name" do
+      @category = Category.new(:name => "execution")
+      @category.save!
 
+      @product = Product.new(:name => nil, :price => 999999, :quantity => 1, :category_id => @category.id)
+      @product.save
+      expect(@product.errors.full_messages).to match_array(["Name can't be blank"])
+    end
 
+    it "absence of product price" do
+      @category = Category.new(:name => "execution")
+      @category.save!
 
-
+      @product = Product.new(:name => "electric chair", :quantity => 1, :category_id => @category.id)
+      @product.save
+      expect(@product.errors.full_messages).to match_array(["Price cents is not a number", "Price is not a number", "Price can't be blank"])
+    end
 
   end
 end
