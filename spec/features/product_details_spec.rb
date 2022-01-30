82050c5eb23196cfb14f0d03e3fdc1bce0e042a7
RiazK <RiazCKhan@gmail.com>
2022-01-30 14:09:12 -0500

    Complete visitor clicks on single product test

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -4,12 +4,14 @@ require 'rails_helper'
 #   pending "add some scenarios (or delete) #{__FILE__}"
 # end
 
-RSpec.feature "Visitor navigates to product page from home page", type: :feature, js: true do
+RSpec.feature "Visitor navigates product page (home page)", type: :feature, js: true do
 
   # SETUP
+
+  # Create Category App
   before :each do
     @category = Category.create! name: 'Apparel'
-
+  # Add 10 items to the Category  
     10.times do |n|
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
@@ -21,14 +23,33 @@ RSpec.feature "Visitor navigates to product page from home page", type: :feature
     end
   end
 
-scenario "They see all products" do
+scenario "They click on one product" do
   # ACT
   visit root_path
-
   # DEBUG / VERIFY
+  
+  expect(page).to have_css 'article.product', count: 10
+  
+  # product = page.find(:xpath, "/html/body/main/section/div/article[1]/header/a/h4")
+
+  product_img = page.find(:xpath, "./html/body/main/section/div/article[1]/header/a/img")
+
+  product_img.click
+
+  sleep(3)
+  
   save_screenshot
 
-  expect(page).to have_css 'article.product', count: 10
-end
+  end
 
 end
+
+
+# Visit Home
+# Locate 'Product' in Nav
+# Select Product
+# expect page
+
+# Locate product item in _product partial
+# Select item
+# expect item
